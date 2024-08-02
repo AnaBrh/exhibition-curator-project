c5ecff20f05ceb0b6243353f5dd40a2a4e0f60db
anabrh <anamariebrh@gmail.com>
2024-08-02 16:59:02 +0100

    removed route for individual artwork

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import ExhibitionPage from './ExhibitionPage';
-import ArtworkDetailPage from './ArtworkDetailPage';
 
 function App() {
-  const [artworks, setArtworks] = useState([]);
-
-  useEffect(() => {
-    const savedExhibition = sessionStorage.getItem('exhibition');
-    if (savedExhibition) {
-      setArtworks(JSON.parse(savedExhibition));
-    }
-  }, []);
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage setArtworks={setArtworks} />} />
-        <Route path="/exhibition" element={<ExhibitionPage artworks={artworks} />} />
-        <Route path="/artwork/:source/:id" element={<ArtworkDetailPage />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/exhibition" element={<ExhibitionPage />} />
       </Routes>
     </Router>
   );
