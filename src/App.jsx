9bce1e28d6dc5d68b796e5b8c58b0de2492eb516
anabrh <anamariebrh@gmail.com>
2024-08-03 14:30:21 +0100

    import style sheets

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import ExhibitionPage from './pages/ExhibitionPage';
+import './styles/App.css'
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Modal from 'react-modal'
+import './styles/index.css'
 
 Modal.setAppElement('#root');
 
