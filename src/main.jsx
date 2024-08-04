30d239515a66428872f80cec882c441c5e03f965
anabrh <anamariebrh@gmail.com>
2024-08-04 22:08:21 +0100

    added context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Modal from 'react-modal'
-import './styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Modal from "react-modal";
+import { ExhibitionProvider } from "./ExhibitionContext.jsx";
+import "./styles/index.css";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ExhibitionProvider>
     <App />
-)
+  </ExhibitionProvider>
+);
